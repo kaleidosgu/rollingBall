5c2a797c8b540d713ef7ef18de8fa4d140459b3d
kaleidos <gjcaoyi@163.com>
2017-08-12 09:55:50 +0800

    游戏简单框架搭建

---
+++ RollingBall/Assets/Script/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform leftTrans;
+    public Transform rightTrans;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float newX = (leftTrans.position.x + rightTrans.position.x) / 2;
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+	}
+}
---
+++ RollingBall/Assets/Script/CharacterController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+    public float moveSpeed;
+    public float jumpSpeed;
+    public KeyCode rightDir;
+    public KeyCode leftDir;
+    public KeyCode jumpAction;
+
+    private Rigidbody2D rigid;
+    // Use this for initialization
+    void Start () {
+        rigid = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        bool bLeft = Input.GetKey(leftDir);
+        bool bRight = Input.GetKey(rightDir);
+        bool bJump = Input.GetKey(jumpAction);
+
+        if( bLeft == true )
+        {
+            rigid.AddForce(Vector2.left * moveSpeed );
+        }
+        if( bRight == true )
+        {
+            rigid.AddForce(Vector2.right * moveSpeed);
+        }
+
+        if( bJump == true )
+        {
+            rigid.AddForce(Vector2.up * jumpSpeed);
+        }
+    }
+}
